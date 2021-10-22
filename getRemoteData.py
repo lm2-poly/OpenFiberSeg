7cc5c1436109a1a5743c0c649cb580a8e51d2979
FacundoLM2 <facundo.sosarey@outlook.com>
2021-10-22 17:01:05 -0400

    initial commit

--- getRemoteData.py
+++ getRemoteData.py
@@ -8,6 +8,6 @@ if not os.path.isdir(targetPath):
 else:
     raise IOError("Target path already exists at: {}".format(targetPath))
 
-Repo.clone_from("https://github.com/FacundoLM2/RPi_timelapse", targetPath)
+Repo.clone_from("https://github.com/lm2-poly/OpenFiberSeg_TomographicData.git", targetPath)
 
 print("Data pulled from remote repository")
