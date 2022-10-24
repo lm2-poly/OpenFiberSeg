39284862f3caeee56c975bfef62b78826acfa127
FacundoLM2 <facundo.sosa-rey@polymtl.ca>
2022-10-24 10:15:24 -0400

    fix type conversion in cpp files

--- InsegtLibraries/texture_gui/code/functions/compile_mex_functions.m
+++ InsegtLibraries/texture_gui/code/functions/compile_mex_functions.m
@@ -1,8 +1,8 @@
 % compile mex files
 mex -largeArrayDims biadjacency_matrix.cpp
-mex build_km_tree.cpp % based on Euclidean distance
-mex search_km_tree.cpp % based on Euclidean distance
-mex build_km_tree_xcorr.cpp % based on normalized cross correlation
-mex search_km_tree_xcorr.cpp % based on normalized cross correlation
-mex probability_search_km_tree.cpp % based on normalized cross correlation
+mex -compatibleArrayDims build_km_tree.cpp % based on Euclidean distance
+mex -compatibleArrayDims search_km_tree.cpp % based on Euclidean distance
+mex -compatibleArrayDims build_km_tree_xcorr.cpp % based on normalized cross correlation
+mex -compatibleArrayDims search_km_tree_xcorr.cpp % based on normalized cross correlation
+mex -compatibleArrayDims probability_search_km_tree.cpp % based on normalized cross correlation
 
